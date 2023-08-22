cdc9bbf2d250b11d14ce242678d48fcc9b6234ef
Vityk18 <vityk18@gmail.com>
2023-08-22 12:04:57 +0300

    hw19 trst

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Main from "./pages/main/Main.js";
+import Posts from "./pages/posts/Posts.js";
+import Photos from "./pages/photos/Photos";
+import Contacts from "./pages/contacts/contacts";
+
+function App() {
+  return (
+
+
+
+
+
+
+
+    <Router>
+    <div>
+      <nav className="navigation">
+        <Link to="/">Main</Link>
+        <Link to="/posts">Posts</Link>
+        <Link to="/photos">Photos</Link>
+        <Link to="/contacts">Contacts</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/photos" element={<Photos />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/main/Main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./main.css"
+
+const Main = () => {
+  return <h1>Project X</h1>;
+};
+
+export default Main;
