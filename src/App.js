ca3cca1347ce51ef56cb617af1c1f59fa68cac4f
Vityk18 <vityk18@gmail.com>
2023-08-22 12:12:16 +0300

    hw19 trst

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Main from "./pages/main/Main.js";
 import Posts from "./pages/posts/Posts.js";
 import Photos from "./pages/photos/Photos";
@@ -7,27 +7,20 @@ import Contacts from "./pages/contacts/contacts";
 
 function App() {
   return (
-
-
-
-
-
-
-
     <Router>
-    <div>
-      <nav className="navigation">
-        <Link to="/">Main</Link>
-        <Link to="/posts">Posts</Link>
-        <Link to="/photos">Photos</Link>
-        <Link to="/contacts">Contacts</Link>
-      </nav>
-      <Routes>
-        <Route path="/hw19/" element={<Main />} />
-        <Route path="/hw19/posts" element={<Posts />} />
-        <Route path="/hw19/photos" element={<Photos />} />
-        <Route path="/hw19/contacts" element={<Contacts />} />
-      </Routes>
+      <div>
+        <nav className="navigation">
+          <Link to="/hw19/">Main</Link>
+          <Link to="/hw19/posts">Posts</Link>
+          <Link to="/hw19/photos">Photos</Link>
+          <Link to="/hw19/contacts">Contacts</Link>
+        </nav>
+        <Routes>
+          <Route path="/hw19/" element={<Main />} />
+          <Route path="/hw19/posts" element={<Posts />} />
+          <Route path="/hw19/photos" element={<Photos />} />
+          <Route path="/hw19/contacts" element={<Contacts />} />
+        </Routes>
       </div>
     </Router>
   );
