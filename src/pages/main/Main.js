0509fc3f960516f1554fec7cd4130a2c08e775c8
Vityk18 <vityk18@gmail.com>
2023-08-22 13:27:41 +0300

    hw19

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -7,7 +7,8 @@ const Main = () => {
   return (
 <div className="main">
   <h1>AC DC</h1>
-  <img  src="https://extrachill.com/wp-content/uploads/2021/12/acdc-high-voltage-logo-1024x1024.jpg"/>
+  <img  src="https://extrachill.com/wp-content/uploads/2021/12/acdc-high-voltage-logo-1024x1024.jpg"
+  alt="Acdc"/>
 </div>
 )
 
